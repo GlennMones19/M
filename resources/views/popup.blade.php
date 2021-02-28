070ddd5cd75a5d39adbc889b1dcc5afb3f1942a1
LeoGlenn <leoglennmones0@gmail.com>
2021-02-28 20:28:53 +0800

    asd

--- resources/views/popup.blade.php
+++ resources/views/popup.blade.php
@@ -1,11 +1,11 @@
-<div class="modal fade" id="myModal">
+<div class="modal fade2" id="myModal">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
         <div class="modal-body">
             <button type="button" class="close" data-dismiss="modal">×</button>
             <div class="d-flex justify-content-center flex-column align-items-center p-5">
                <h3 class="py-2">Sign Up Now</h3>
-               <button class="btn btn-warning">Register Now...</button>
+               <button class="btn btn-warning text-white p-2">Register Now...</button>
            </div>
         </div>
     </div>
